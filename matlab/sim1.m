8a377b52269b835694eb3a681ee8578575921be2
Tom Holden <thomas.holden@gmail.com>
2014-07-14 18:35:42 +0100

    Patch to increase the robustness of sim1.m

--- matlab/sim1.m
+++ matlab/sim1.m
@@ -146,9 +146,9 @@ for iter = 1:options_.simul.maxit
 
     if endogenous_terminal_period && iter>1
         dy = ZERO;
-        dy(1:i_rows(end)) = -A(1:i_rows(end),1:i_rows(end))\res(1:i_rows(end));
+        dy(1:i_rows(end)) = -lin_solve( A(1:i_rows(end),1:i_rows(end)), res(1:i_rows(end)) );
     else
-        dy = -A\res;
+        dy = -lin_solve( A, res );
     end
     
     Y(i_upd) =   Y(i_upd) + dy;
@@ -197,3 +197,110 @@ elseif ~stop
 end
 disp (['-----------------------------------------------------']) ;
 skipline();
+end
+
+function [ x, flag, relres ] = lin_solve2( A, b )
+    if norm( b ) < sqrt( eps )
+        x = 0;
+        flag = 0;
+        relres = 0;
+        return
+    end
+    
+    WarningState = warning( 'off', 'all' );
+    
+    try % wrap in a try-catch block to ensure that the WarningState is always reset
+    
+        x = A\b;
+        x( ~isfinite( x ) ) = 0;
+        relres = norm( b - A * x ) / norm( b );
+        flag = relres > 1e-6;
+        warning( WarningState );
+        if flag ~= 0
+            warning( 'Failed to find a solution to the linear system' );
+        end
+        
+    catch Error
+        
+        warning( WarningState );
+        error( Error ); %rethrow
+        
+    end
+end
+
+function [ x, flag, relres ] = lin_solve( A, b )
+    if norm( b ) < sqrt( eps )
+        x = 0;
+        flag = 0;
+        relres = 0;
+        return
+    end
+    
+    WarningState = warning( 'off', 'all' );
+    
+    try % wrap in a try-catch block to ensure that the WarningState is always reset
+    
+        x = A\b;
+        x( ~isfinite( x ) ) = 0;
+        [ x, flag, relres ] = bicgstab( A, b, [], [], [], [], x ); % returns immediately if x is a solution
+        if flag == 0
+            warning( WarningState );
+            return
+        end
+        
+        disp( relres );
+
+        fprintf( 'Initial bicgstab failed, trying alternative start point.\n' );
+        old_x = x;
+        old_relres = relres;
+        [ x, flag, relres ] = bicgstab( A, b );
+        if flag == 0
+            warning( WarningState );
+            return
+        end
+
+        fprintf( 'Alternative start point also failed with bicgstab, trying gmres.\n' );
+        if old_relres < relres
+            x = old_x;
+        end
+        [ x, flag, relres ] = gmres( A, b, [], [], [], [], [], x );
+        if flag == 0
+            warning( WarningState );
+            return
+        end
+
+        fprintf( 'Initial gmres failed, trying alternative start point.\n' );
+        old_x = x;
+        old_relres = relres;
+        [ x, flag, relres ] = gmres( A, b );
+        if flag == 0
+            warning( WarningState );
+            return
+        end
+
+        fprintf( 'Alternative start point also failed with gmres, using the (SLOW) Moore-Penrose Pseudo-Inverse.\n' );
+        if old_relres < relres
+            x = old_x;
+            relres = old_relres;
+        end
+        old_x = x;
+        old_relres = relres;
+        x = pinv( full( A ) ) * b;
+        relres = norm( b - A * x ) / norm( b );
+        if old_relres < relres
+            x = old_x;
+            relres = old_relres;
+        end
+        flag = relres > 1e-6;
+        warning( WarningState );
+        if flag ~= 0
+            warning( 'Failed to find a solution to the linear system' );
+        end
+        
+    catch Error
+        
+        warning( WarningState );
+        error( Error ); %rethrow
+        
+    end
+end
