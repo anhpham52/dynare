f86cf5069469acadfd7caeb22761ca959ab9b217
Tom Holden <thomas.holden@gmail.com>
2014-07-15 18:50:33 +0100

    Minimal change to sim1.m to increase robustness
    Previous commit used the algorithm now in lin_solve_robust.m by default
    for solving the linear system in the Newton update in sim1.m.
    This commit switches to a much simpler algorithm which just does
    mldivide, then if that generates any nans or infs, replaces these
    elements by zero, and tests if that is a solution.

--- matlab/sim1.m
+++ matlab/sim1.m
@@ -199,11 +199,9 @@ disp (['-----------------------------------------------------']) ;
 skipline();
 end
 
-function [ x, flag, relres ] = lin_solve2( A, b )
-    if norm( b ) < sqrt( eps )
+function x = lin_solve( A, b )
+    if norm( b ) < sqrt( eps ) % then x = 0 is a solution
         x = 0;
-        flag = 0;
-        relres = 0;
         return
     end
     
@@ -214,10 +212,8 @@ function [ x, flag, relres ] = lin_solve2( A, b )
         x = A\b;
         x( ~isfinite( x ) ) = 0;
         relres = norm( b - A * x ) / norm( b );
-        flag = relres > 1e-6;
-        warning( WarningState );
-        if flag ~= 0
-            warning( 'Failed to find a solution to the linear system' );
+        if relres > 1e-6
+            fprintf( 'WARNING : Failed to find a solution to the linear system.\n' );
         end
         
     catch Error
@@ -226,81 +222,8 @@ function [ x, flag, relres ] = lin_solve2( A, b )
         error( Error ); %rethrow
         
     end
+    
+    warning( WarningState );
+    
 end
 
-function [ x, flag, relres ] = lin_solve( A, b )
-    if norm( b ) < sqrt( eps )
-        x = 0;
-        flag = 0;
-        relres = 0;
-        return
-    end
-    
-    WarningState = warning( 'off', 'all' );
-    
-    try % wrap in a try-catch block to ensure that the WarningState is always reset
-    
-        x = A\b;
-        x( ~isfinite( x ) ) = 0;
-        [ x, flag, relres ] = bicgstab( A, b, [], [], [], [], x ); % returns immediately if x is a solution
-        if flag == 0
-            warning( WarningState );
-            return
-        end
-        
-        disp( relres );
-
-        fprintf( 'Initial bicgstab failed, trying alternative start point.\n' );
-        old_x = x;
-        old_relres = relres;
-        [ x, flag, relres ] = bicgstab( A, b );
-        if flag == 0
-            warning( WarningState );
-            return
-        end
-
-        fprintf( 'Alternative start point also failed with bicgstab, trying gmres.\n' );
-        if old_relres < relres
-            x = old_x;
-        end
-        [ x, flag, relres ] = gmres( A, b, [], [], [], [], [], x );
-        if flag == 0
-            warning( WarningState );
-            return
-        end
-
-        fprintf( 'Initial gmres failed, trying alternative start point.\n' );
-        old_x = x;
-        old_relres = relres;
-        [ x, flag, relres ] = gmres( A, b );
-        if flag == 0
-            warning( WarningState );
-            return
-        end
-
-        fprintf( 'Alternative start point also failed with gmres, using the (SLOW) Moore-Penrose Pseudo-Inverse.\n' );
-        if old_relres < relres
-            x = old_x;
-            relres = old_relres;
-        end
-        old_x = x;
-        old_relres = relres;
-        x = pinv( full( A ) ) * b;
-        relres = norm( b - A * x ) / norm( b );
-        if old_relres < relres
-            x = old_x;
-            relres = old_relres;
-        end
-        flag = relres > 1e-6;
-        warning( WarningState );
-        if flag ~= 0
-            warning( 'Failed to find a solution to the linear system' );
-        end
-        
-    catch Error
-        
-        warning( WarningState );
-        error( Error ); %rethrow
-        
-    end
-end
