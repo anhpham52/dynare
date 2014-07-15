7ec94c6224bdb3c9c0f0a14c7229ef0e830e21e1
Tom Holden <thomas.holden@gmail.com>
2014-07-15 18:54:13 +0100

    Moved lin_solve to new file

--- matlab/sim1.m
+++ matlab/sim1.m
@@ -197,33 +197,3 @@ elseif ~stop
 end
 disp (['-----------------------------------------------------']) ;
 skipline();
-end
-
-function x = lin_solve( A, b )
-    if norm( b ) < sqrt( eps ) % then x = 0 is a solution
-        x = 0;
-        return
-    end
-    
-    WarningState = warning( 'off', 'all' );
-    
-    try % wrap in a try-catch block to ensure that the WarningState is always reset
-    
-        x = A\b;
-        x( ~isfinite( x ) ) = 0;
-        relres = norm( b - A * x ) / norm( b );
-        if relres > 1e-6
-            fprintf( 'WARNING : Failed to find a solution to the linear system.\n' );
-        end
-        
-    catch Error
-        
-        warning( WarningState );
-        error( Error ); %rethrow
-        
-    end
-    
-    warning( WarningState );
-    
-end
-
