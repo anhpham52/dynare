0c533744750ed3247fd25c43b1fe82c1fea64bbb
Tom Holden <thomas.holden@gmail.com>
2015-06-17 11:43:43 +0100

    dyn_mex bug fix

--- matlab/utilities/general/dyn_mex.m
+++ matlab/utilities/general/dyn_mex.m
@@ -47,17 +47,17 @@ end
 if ~exist('OCTAVE_VERSION')
     % Some mex commands are enclosed in an eval(), because otherwise it will make Octave fail
     if ispc
-      if msvc
+      if strcmp( win_compiler, 'msvc' )
           % MATLAB/Windows + Microsoft Visual C++
           eval(['mex -O LINKFLAGS="$LINKFLAGS /export:Dynamic" ' basename '_dynamic.c ' basename '_dynamic_mex.c'])
-          eval(['mex -O LINKFLAGS="$LINKFLAGS /export:Dynamic" ' basename '_static.c ' basename '_static_mex.c'])
-      elseif cygwin
+          eval(['mex -O LINKFLAGS="$LINKFLAGS /export:Static" ' basename '_static.c ' basename '_static_mex.c'])
+      elseif strcmp( win_compiler, 'cygwin' )
           % MATLAB/Windows + Cygwin g++
           eval(['mex -O PRELINK_CMDS1="echo EXPORTS > mex.def & echo ' ...
                 'mexFunction >> mex.def & echo Dynamic >> mex.def" ' ...
                 basename '_dynamic.c ' basename '_dynamic_mex.c'])
           eval(['mex -O PRELINK_CMDS1="echo EXPORTS > mex.def & echo ' ...
-                'mexFunction >> mex.def & echo Dynamic >> mex.def" ' ...
+                'mexFunction >> mex.def & echo Static >> mex.def" ' ...
                 basename '_static.c ' basename '_static_mex.c'])
       else
         error(['When using the USE_DLL option, you must give either ' ...
