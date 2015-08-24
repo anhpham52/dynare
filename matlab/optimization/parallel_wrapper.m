5b42e03ef50dc26d8fcd950bf7b9e71bc021bb04
Tom Holden <thomas.holden@gmail.com>
2015-08-24 21:38:36 +0100

    parallel wrapper robustness

--- matlab/optimization/parallel_wrapper.m
+++ matlab/optimization/parallel_wrapper.m
@@ -1,7 +1,7 @@
 function RV = parallel_wrapper( objective_function, XV, varargin )
     n = size( XV, 2 );
     RV = zeros( 1, n );
-    for i = 1 : n
+    parfor i = 1 : n
         R = objective_function( XV( :, i ), varargin{:} ); %#ok<PFBNS>
         if isempty( R ) || ~isfinite( R( 1 ) )
             R = 1e12;
