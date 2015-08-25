6d616852a676b92a273bb844ce35858bbc9711bc
tholden <thomas.holden@gmail.com>
2015-08-25 13:22:25 -0700

    parallel resampling for CMAES

--- matlab/optimization/cmaes.m
+++ matlab/optimization/cmaes.m
@@ -923,54 +923,57 @@ while isempty(stopflag)
 
   % non-parallel evaluation and remaining NaN-values
   % set also the reevaluated solution to NaN
-  fitness.raw(lambda + find(isnan(fitness.raw(1:noiseReevals)))) = NaN;  
-  for k=find(isnan(fitness.raw)), 
-    % fitness.raw(k) = NaN; 
-    tries = flgEvalParallel;  % in parallel case this is the first re-trial
-    % Resample, until fitness is not NaN
-    while isnan(fitness.raw(k))
-      if k <= lambda  % regular samples (not the re-evaluation-samples)
-        arz(:,k) = randn(N,1); % (re)sample
+  fitness.raw(lambda + find(isnan(fitness.raw(1:noiseReevals)))) = NaN;
+  while any(isnan(fitness.raw))
+      current_kvec = find(isnan(fitness.raw));
+      for k=current_kvec
+        % fitness.raw(k) = NaN; 
+        % Resample, until fitness is not NaN
+          if k <= lambda  % regular samples (not the re-evaluation-samples)
+            arz(:,k) = randn(N,1); % (re)sample
 
-        if flgDiagonalOnly  
-          arx(:,k) = xmean + sigma * diagD .* arz(:,k);              % Eq. (1)
-        else
-          arx(:,k) = xmean + sigma * (BD * arz(:,k));                % Eq. (1)
-        end
-      else % re-evaluation solution with index > lambda
-        if flgDiagonalOnly  
-          arx(:,k) = arx(:,k-lambda) + (noiseEpsilon * sigma) * diagD .* randn(N,1);
-        else
-          arx(:,k) = arx(:,k-lambda) + (noiseEpsilon * sigma) * (BD * randn(N,1));
-        end
+            if flgDiagonalOnly  
+              arx(:,k) = xmean + sigma * diagD .* arz(:,k);              % Eq. (1)
+            else
+              arx(:,k) = xmean + sigma * (BD * arz(:,k));                % Eq. (1)
+            end
+          else % re-evaluation solution with index > lambda
+            if flgDiagonalOnly  
+              arx(:,k) = arx(:,k-lambda) + (noiseEpsilon * sigma) * diagD .* randn(N,1);
+            else
+              arx(:,k) = arx(:,k-lambda) + (noiseEpsilon * sigma) * (BD * randn(N,1));
+            end
+          end
+
+          % You may handle constraints here. You may either resample
+          % arz(:,k) and/or multiply it with a factor between -1 and 1
+          % (the latter will decrease the overall step size) and
+          % recalculate arx accordingly. Do not change arx or arz in any
+          % other way.
+
+          if ~bnd.isactive
+            arxvalid(:,k) = arx(:,k);
+          else
+            arxvalid(:,k) = xintobounds(arx(:,k), lbounds, ubounds);
+          end
+          % You may handle constraints here.  You may copy and alter
+          % (columns of) arxvalid(:,k) only for the evaluation of the
+          % fitness function. arx should not be changed.
       end
-      
-      % You may handle constraints here. You may either resample
-      % arz(:,k) and/or multiply it with a factor between -1 and 1
-      % (the latter will decrease the overall step size) and
-      % recalculate arx accordingly. Do not change arx or arz in any
-      % other way.
- 
-      if ~bnd.isactive
-        arxvalid(:,k) = arx(:,k);
+      if flgEvalParallel
+          fitness.raw(current_kvec) = feval(fitfun, arxvalid(:,current_kvec), varargin{:}); 
       else
-        arxvalid(:,k) = xintobounds(arx(:,k), lbounds, ubounds);
+          for k=current_kvec
+              fitness.raw(k) = feval(fitfun, arxvalid(:,k), varargin{:}); 
+          end
       end
-      % You may handle constraints here.  You may copy and alter
-      % (columns of) arxvalid(:,k) only for the evaluation of the
-      % fitness function. arx should not be changed.
-      fitness.raw(k) = feval(fitfun, arxvalid(:,k), varargin{:}); 
-      tries = tries + 1;
-      if isnan(fitness.raw(k))
-	countevalNaN = countevalNaN + 1;
+      for k=current_kvec
+          if isnan(fitness.raw(k))
+          	  countevalNaN = countevalNaN + 1;
+          else
+              counteval = counteval + 1; % retries due to NaN are not counted
+          end
       end
-      if mod(tries, 100) == 0
-	warning([num2str(tries) ...
-                 ' NaN objective function values at evaluation ' ...
-                 num2str(counteval)]);
-      end
-    end
-    counteval = counteval + 1; % retries due to NaN are not counted
   end
 
   fitness.sel = fitness.raw; 
