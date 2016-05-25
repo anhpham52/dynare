a20bbbc297c1b7e7c5e840e96c3897acad7d271d
tholden <thomas.holden@gmail.com>
2016-05-25 18:30:41 +0100

    parallel_wrapper updates for new global

--- matlab/optimization/parallel_wrapper.m
+++ matlab/optimization/parallel_wrapper.m
@@ -1,8 +1,18 @@
 function RV = parallel_wrapper( objective_function, XV, varargin )
     n = size( XV, 2 );
     RV = zeros( 1, n );
+    
+    global objective_function_penalty_base
+
+    if isempty( objective_function_penalty_base ) || objective_function_penalty_base <= 1
+        objective_function_penalty_base = Inf;
+    end
+    
+    in_objective_function_penalty_base = objective_function_penalty_base;
+    
     parfor i = 1 : n
         R = [];
+        SetGlobals( in_objective_function_penalty_base );
         WarningState = warning( 'off', 'all' );
         try
             R = objective_function( XV( :, i ), varargin{:} ); %#ok<PFBNS>
@@ -14,4 +24,9 @@ function RV = parallel_wrapper( objective_function, XV, varargin )
         end
         RV( i ) = real( R( 1 ) );
     end
+end
+
+function SetGlobals( in_objective_function_penalty_base )
+    global objective_function_penalty_base
+    objective_function_penalty_base = in_objective_function_penalty_base;
 end
