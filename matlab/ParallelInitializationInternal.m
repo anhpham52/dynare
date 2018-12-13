55e25050bae7b37f032551ccfe1f61dc039efe97
Tom Holden <thomas.holden@gmail.com>
2018-12-13 16:12:39 +0100

    initialize random number generator on workers

--- matlab/ParallelInitializationInternal.m
+++ matlab/ParallelInitializationInternal.m
@@ -1,12 +1,15 @@
 function ParallelInitializationInternal( varargin )
+    Seed = randi( ceil( intmax / 2 ) );
     if nargin == 0
         spmd
             SetGlobalWorkerNumber( labindex );
+            rng( Seed + labindex );
         end
     else
         in_options_ = varargin{ 1 };
         spmd
             SetGlobalWorkerNumber( labindex );
+            rng( Seed + labindex );
             Set_options_( in_options_ );
         end
     end
