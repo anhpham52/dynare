d92f205c39c75eca7e15446b54f0692028c13dbf
Tom Holden <thomas.holden@gmail.com>
2019-01-30 17:26:29 +0100

    function to find a step size at which x looks like a maximum

---
+++ matlab/FindStepSize.m
@@ -0,0 +1,26 @@
+function h = FindStepSize( f, x, h )
+    nx = length( x );
+    sreps = sqrt( eps );
+    if nargin < 3
+        h = sreps;
+    end
+    h = ones( nx, 1 ) .* h;
+    fx = f( x );
+    parfor i = 1 : nx
+        WarningState = warning( 'off', 'all' );
+        xi = x( i );
+        hi = max( [ h( i ), sreps, abs( sreps * xi ), eps( abs( xi ) ) ] );
+        while true
+            if ( f( SetElement( x, i, xi + hi ) ) > fx ) && ( f( SetElement( x, i, xi - hi ) ) > fx ) %#ok<PFBNS>
+                break
+            end
+            hi = 2 * hi;
+        end
+        h( i ) = hi;
+        warning( WarningState );
+    end
+end
+
+function x = SetElement( x, i, xi )
+    x( i ) = xi;
+end
