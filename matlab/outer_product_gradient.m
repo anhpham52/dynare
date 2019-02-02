5526faa617c1b75b033c9a82ceadf83a70aa7eea
Tom Holden <thomas.holden@gmail.com>
2019-02-02 21:09:05 +0100

    zero problematic elements of dscores

--- matlab/outer_product_gradient.m
+++ matlab/outer_product_gradient.m
@@ -11,6 +11,14 @@ function hessian_mat = outer_product_gradient( func, x, dataset, varargin )
 
     dscores = GetJacobian( @( x ) get_lik_components( x, prior_weights, func, dataset, varargin{:} ), x, length( prior_weights ) );
     
+    bad_params = find( ~( all( isfinite( dscores ) ) ) );
+    
+    if ~isempty( bad_params )
+        disp( 'Results may be unreliable due to non-finite Hessian elements with respect to the following parameters:' );
+        disp( bad_params );
+        dscores( :, bad_params ) = 0;
+    end
+    
     hessian_mat = dscores' * dscores;
 
     hessian_mat = 0.5 * ( hessian_mat + hessian_mat' );
