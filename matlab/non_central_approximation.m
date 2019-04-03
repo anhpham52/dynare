68d74b790a61ca585775bfa74dea62584eae2664
Tom Holden <thomas.holden@gmail.com>
2019-04-03 18:44:53 +0200

    update nlma, use new deflect calling convention

--- matlab/non_central_approximation.m
+++ matlab/non_central_approximation.m
@@ -1,20 +1,20 @@
 function [ info, M, options, oo ] = non_central_approximation( M, options, oo )
     options.original_order = options.order;
-    deflect_ = compute_deflected_linear_approximation( M, options, oo, options.non_central_approximation );
+    [ deflect, M, oo ] = compute_deflected_linear_approximation( M, options, oo, options.non_central_approximation );
     options.order = 1;
-    oo.dr.ys = deflect_.y;
-    oo.steady_state = deflect_.y;
-    oo.dr.ghx = deflect_.y_x;
-    oo.dr.ghu = deflect_.y_u;
+    oo.dr.ys = deflect.y;
+    oo.steady_state = deflect.y;
+    oo.dr.ghx = deflect.y_x;
+    oo.dr.ghu = deflect.y_u;
     
     info = 0;
-    if any( isnan( deflect_.y ) )
+    if any( isnan( deflect.y ) )
         info = 12345;
     end
-    if any( any( isnan( deflect_.y_x ) ) )
+    if any( any( isnan( deflect.y_x ) ) )
         info = 12345;
     end
-    if any( any( isnan( deflect_.y_u ) ) )
+    if any( any( isnan( deflect.y_u ) ) )
         info = 12345;
     end
 end
